77358e0935b2c8b0b0571be73052c35accb97ac4
julcin <logic1517@gmail.com>
2021-04-07 06:42:08 +0300

    added js and css

---
+++ js/script.js
@@ -0,0 +1,76 @@
+"use strict";
+
+//read more at https://unsplash.com/documentation
+
+const form = document.querySelector("#search-form");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const keyword = e.target[0].value;
+
+  //sending request to api with keyword from searchbox as parameter
+  //and setting header with authorization key
+  fetch(
+    `https://api.unsplash.com/search/photos/?query=${keyword}&per_page=12`,
+    {
+      headers: {
+        Authorization: "Client-ID _nljZlZE2dNQlnThC-THfGmtcgkjEOiL0u5j2ke6ERA",
+      },
+    }
+  )
+    .then(response => response.json())
+    .then(data => {
+      displayResult(keyword);
+
+      //selecting only data needed form api reponse
+      const images = data.results.map(item => {
+        return {
+          alt: item.alt_description,
+          url: item.urls.regular,
+        };
+      });
+
+      displayimages(images);
+
+      //clearing search box
+      e.target[0].value = "";
+    })
+    .catch(e => console.log(e));
+});
+
+function displayResult(keyword) {
+  const resultContainer = document.querySelector("#result-container");
+  //removing previous result if there is any
+  if (resultContainer.firstChild) {
+    resultContainer.firstChild.remove();
+  }
+  const title = document.createElement("h2");
+  title.appendChild(document.createTextNode(`Images found for "${keyword}":`));
+  resultContainer.appendChild(title);
+}
+
+function displayimages(images) {
+  const galeryContianer = document.querySelector("#image-container");
+
+  //clearing previous image results
+  while (galeryContianer.firstChild) {
+    galeryContianer.firstChild.remove();
+  }
+
+  //filling gallery container with elements from maped data
+  for (let imageData of images) {
+    const image = document.createElement("img");
+    image.src = imageData.url;
+    image.alt = imageData.alt;
+
+    const imageLink = document.createElement("a");
+    imageLink.target = "_blank";
+    imageLink.href = imageData.url;
+    imageLink.appendChild(image);
+
+    const imageContainer = document.createElement("div");
+    imageContainer.appendChild(imageLink);
+
+    galeryContianer.appendChild(imageContainer);
+  }
+}
